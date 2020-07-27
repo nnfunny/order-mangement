fa5cde5f86295b821eb60b08073ffbc79c9f874b
Nam Nguyen <nguyenvietnam2401@gmail.com>
2020-07-27 17:59:48 +1000

    Updated

--- server/dist/credential.js
+++ server/dist/credential.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.URI = exports.PASSWORD = exports.USERNAME = void 0;
-exports.USERNAME = "namvnngu";
-exports.PASSWORD = "vietnam225499";
-exports.URI = `mongodb+srv://${exports.USERNAME}:${exports.PASSWORD}@node.d5frw.mongodb.net/learning?retryWrites=true&w=majority`;
+exports.USERNAME = "";
+exports.PASSWORD = "";
+exports.URI = "";
